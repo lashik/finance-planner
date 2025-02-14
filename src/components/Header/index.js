4edb6884a97c03c27a2376b1e9c6080c66016bd7
Kashik Sredharan <kashik.sredharan@gmail.com>
2025-02-14 22:52:55 +0530

    main

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,9 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import PropTypes from "prop-types";
 import styles from "./index.module.scss";
 import { setGlobalVar, globalVar } from "db";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+
 function Header(props) {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
@@ -50,9 +49,9 @@ function Header(props) {
               <ul>
                 <li >
                   <img className={styles.settingsIcon1} src={"/assets/42c00a9355f73928c69d06a8a8bc775c.svg"} alt="alt text" />
-                  <a href="/" onClick={()=>{console.log("Logout button clicked")}}>Settings</a></li>
+                  <a href="/">Settings</a></li>
                 <li>
-                  <img className={styles.notificationIcon} src={"/assets/5c3de22dde775d9719fce3167251562b.svg"} alt="alt text" /><a >Notifications</a></li>
+                  <img className={styles.notificationIcon} src={"/assets/5c3de22dde775d9719fce3167251562b.svg"} alt="alt text" /><a href="/" >Notifications</a></li>
                 
                 <li><button  className={styles.logoutButton} onClick={handleLogout}>
                     <img className={styles.settingsIcon1} src={"/assets/logout (1).png"} alt="Logout" />
